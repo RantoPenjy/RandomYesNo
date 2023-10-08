be5fd5e7b9a447e1d844364011affecd6aa22b40
Rakotondramanana Ranto <rakotondramanana.rantoniaina@gmail.com>
2023-10-08 16:47:23 +0300

    Add english and french translations

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 import './public/style.css';
-import customConfetti from './public/confetti.js';
+import customConfetti from './public/confetti';
+import { translation } from './public/translation';
 import $ from 'jquery';
 
 const api_url = 'https://yesno.wtf/api';
 let randomBtn = $('.random-button');
+let currentLanguage = 'en';
 
 $(document).ready(function () {
+    function changeLanguage(lang) {
+        currentLanguage = lang;
+        $('.welcome-message').text(translation[lang]['Welcome']);
+        $('.error-message').text(translation[lang]['Error']);
+    }
+
     randomBtn.on('click', function () {
         let randomize = $(this).find('i.fa-refresh');
         let handIconRandomize = $('.button span');
@@ -18,36 +26,50 @@ $(document).ready(function () {
             .then(data => {
                 randomize.removeClass('spin-animation');
                 welcome.hide();
-                errorMessage.html('');
+                errorMessage.html(translation[currentLanguage]['Error']);
 
-                let answer = data.answer.toUpperCase();
-                let imageAlt =
-                    'Gif animation for "' + answer + '"';
-
-                if (data.answer === 'yes'){
+                if (data.answer === 'yes') {
+                    data.answer = translation[currentLanguage]['Yes'];
                     customConfetti();
+                } else {
+                    data.answer = translation[currentLanguage]['No'];
                 }
 
+                let answer = data.answer.toUpperCase();
+                let imageAlt = 'Gif animation for "' + answer + '"';
+
                 $('.card').html(`
-                  <div class="answer">
-                    <span class="animate__animated animate__shakeX animate__infinite">👉</span>
+                    <div class="answer">
+                        <span class="animate__animated animate__shakeX animate__infinite">👉</span>
                         <b>${answer}</b>
-                    <span class="animate__animated animate__shakeX animate__infinite">👈</span>
-                  </div>
-                  <div class="gif-animation">
-                    <img src="${data.image}" alt="${imageAlt}">
-                  </div>
+                        <span class="animate__animated animate__shakeX animate__infinite">👈</span>
+                    </div>
+                    <div class="gif-animation">
+                        <img src="${data.image}" alt="${imageAlt}">
+                    </div>
                 `);
             })
             .catch(error => {
-              randomize.removeClass('spin-animation');
-              errorMessage.html('Error when fetching resources, please check your internet connection');
-              console.error('Error:', error);
+                randomize.removeClass('spin-animation');
+                errorMessage.html(translation[currentLanguage]['Error']).show();
+                console.error('Error:', error);
             });
+    });
+
+    $('.translation-button').on('click', function () {
+        if ($(this).data('lang') === 'en') {
+            $(this).data('lang', 'fr');
+            changeLanguage('fr');
+            $(this).text('EN');
+        } else {
+            $(this).data('lang', 'en');
+            changeLanguage('en');
+            $(this).text('FR');
+        }
     });
 });
 
 const fetchData = async (url) => {
     const response = await fetch(url);
     return response.json();
-}
+};
---
+++ public/translation.js
@@ -0,0 +1,14 @@
+export const translation = {
+  'en': {
+    'Yes': 'Yes',
+    'No': 'No',
+    'Welcome': 'Click the top button on the side to random the answer',
+    'Error': 'Error when fetching resources, please check your internet connection'
+  },
+  'fr': {
+    'Yes': 'Oui',
+    'No': 'Non',
+    'Welcome': 'Cliquez le bouton du haut à coté pour tirer la réponse au hasard',
+    'Error': 'Erreur lors de la récupération des ressources, veuillez vérifier votre connexion Internet'
+  }
+}
